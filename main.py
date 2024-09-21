20d93df1d6e8f8af2d1f77c83da6982597f537da
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-21 15:11:57 +0100

    Summer times

---
+++ main.py
@@ -0,0 +1,5 @@
+temperature = int(input("Enter current temperature:"))
+if temperature > 25:
+    print("It is warm today, you can wear light clothes")
+else:
+    print("It is cold today, you can wear warm clothes") 
